d5a1def4dc5ed54dce87191a278742e262b6a1ae
pedro-phd <pedro_dominici@outlook.com>
2020-03-27 14:22:18 -0300

    front

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
-
+import './App.css'
+import logo from '../assets/logo.svg'
+import buscar from '../assets/search.svg'
 function App() {
   return (
-    <div className="App">
-      <h1>phd</h1>
+    <div className="container">
+      <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet"></link>
+      <img src={logo} alt="logo" id="logo"/>
+      <section>
+        <input placeholder="Nome da doença ..."></input>
+        <button><img src={buscar} alt="buscar"/></button>
+      </section>
+      
     </div>
   );
 }
